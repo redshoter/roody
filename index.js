bff14c97575414cfd5cab5b33ceee5d6e6195315
redshoter <78428341+redshoter@users.noreply.github.com>
2021-02-11 15:13:27 +0100

    Update index.js

--- index.js
+++ index.js
@@ -27,36 +27,85 @@ monitor.on('error', (error) => console.log(error));
 
 //////////////////////////BOT REAL//////////////////////
 
+
+
 var prefix = "="
 
+
 client.on("message", msg => {
-  if (msg.content.startsWith("hola")) {
-    msg.channel.send("Hola, soy el bot de @redshoter si tienes alguna duda puedes poner =help :slight_smile:")
-    }
-    });
+  if (msg.content.startsWith(prefix + "comandos")) {
+    msg.channel.send("`-----COMANDOS DE AYUDA----- =help =comandos -----COMANDOS DE DIVERSION----- UwU -----COMANDOS DE MODERACION----- =ban (@ del ususario + razon)`")
+}
+});
 
 client.on("message", msg => {
   if (msg.content.startsWith(prefix + "help")) {
-    msg.channel.send("Con este bot puedes hacer muchas funciones, puedes ver toda la lista de ellas aqui ")
+    msg.channel.send("Con este bot puedes hacer muchas funciones, puedes ver toda la lista de ellas aqui")
 }
 });
+
+
+client.on("message", msg => {
+  if (msg.content.startsWith("UwU")) {
+    msg.channel.send(":flushed: UwU :flushed:")
+}
+});
+
+
+const ms = require("ms") // Requieres el modulo ms.
+time = "1h"
+setTimeout(async function()  {
+console.log("=tempmute")
+}, ms(time))
+
+client.on("message", async(message) => {
+if(message.author.bot) return;
+if(!message.content.startsWith(prefix)) return;
+
+var args = message.content.slice(prefix.length).trim().split(/ +/g)
+var command = args.shift().toLowerCase()
+
+if(command === "ban"){
+ if(!message.member.permissions.has('BAN_MEMBERS')) return message.channel.send(":x: | ¡No tienes permisos para hacer esto!");
+        
+let member = message.mentions.members.first();
+if(!member) return message.channel.send(":x: | Necesitas mencionar un usuario")
+if(!member.bannable) return message.channel.send(":x: | No puedes banear a ese usuario\n \n· Porque él / ella tiene el **mismo rol** o es **superior a tu rol**")
+
+if(message.guild.owner.id !== message.author.id && member.roles.highest.comparePositionTo(message.member.roles.highest) >= 0) return message.channel.send(":x: | No puedes banear a ese usuario\n \n· Porque él o ella tiene el **mismo rol** o tiene un rol **superior a tu rol**");
+
+if(member.id === message.author.id) return message.channel.send(":x: | ¡No puedes banearte a ti mismo!")
+
+if(member.id === client.user.id) return message.channel.send(":x: | No puedes Banearme 😔")
+
+
+let reason = args[1] ? args.slice(1).join(' ') : ':x: | No especificado'
+
+await member.ban({ reason }).catch(error => message.channel.send(`:x: | No puedo banear a ese usuario\n \nError: **${error}**`));
+  
+let uban = message.mentions.members.first() || message.guild.members.get(args[0])
+
+var embed = new Discord.MessageEmbed()
+   .setAuthor(client.user.username, client.user.avatarURL())
+   .setColor("0x5b00ff")
+   .addField("Autor", message.author.username)
+   .addField("Usuario", member.user.username)
+   .addField("Razon", reason)
+    message.channel.send(embeon)
+}
+})
 
 
 const MusicBot = require('discord-music-system-es'); // requerimos el paquete de música
 
 const bot = new MusicBot({
-    botPrefix: '=', // Ejemplo: !
-    ytApiKey: 'AIzaSyDHsSNuTyniRB1ThcuMz2c17bZUJbdTUvw', // Video de como obtener tu API KEY: https://www.youtube.com/watch?v=VqML5F8hcRQ
-    botClient: client //Tu cliente de Discord. Aquí estamos usando discord.js, por lo que es Discord.Client ()
+    botPrefix: '=', // 
+    ytApiKey: 'AIzaSyDHsSNuTyniRB1ThcuMz2c17bZUJbdTUvw', 
+    botClient: client 
 });
 
-client.on('message', message => { // Cuando un el bot recive un mensaje
-    if(message.content.startsWith(bot.prefix)){ // Si el mensaje empieza con su prefijo
-        bot.onMessage(message); // discord-music-system lee si es un comando de música o no
-    };
-});
+client.login(''); //aqui ponemos el TOKEN de nuestro bot 
 
-client.login('TOKEN'); // Aqui ponemos el token de nuestro bot, obtenlo creando una aplicación en https://discord.com/developers/applications/
 
 
 client.login(process.env.TOKEN);
